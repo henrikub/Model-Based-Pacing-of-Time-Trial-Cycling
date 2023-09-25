27d29f98aa115608e01c27f29c793e00a6461bf7
Henrik Brokke <brokkehenrik@gmail.com>
2023-09-25 11:29:20 +0200

    Add parameters file

---
+++ parameters.py
@@ -0,0 +1,13 @@
+class Cyclist:
+    def __init__(self):
+        self.weight = 80
+        self.height = 180
+        self.cp = 230
+        self.awc = 23000
+
+class Bicycle:
+    def __init__(self):
+        self.weight = 9.25
+        self.wheel_radius = 0.35
+        self.wheel_mass = 1.2
+
