102f8b556a0a5f33221d2066e6acf6317c84c7a4
Henrik Brokke <brokkehenrik@gmail.com>
2023-10-25 15:38:34 +0200

    Add 300w test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from activity_reader import ActivityReader
-act = ActivityReader("FTP_test.tcx")
+act = ActivityReader("300W_test.tcx")
 distance, time, elevation, latitude, longitude, heart_rate, cadence, power, speed = act.get_activity_data()
 
 
@@ -8,20 +8,27 @@ import matplotlib.pyplot as plt
 import sweat
 import numpy as np
 from plotting import *
+from test_processing import *
 
 data = pd.DataFrame(dict(power=power), index=time)
 
-cp = 227
-w_prime = 19000
+cp = 287
+w_prime = 9000
 
-w_bal_waterworth = sweat.w_prime_balance(data["power"], cp=cp, algorithm='waterworth', w_prime=w_prime).to_list()
-w_bal_fsc = sweat.w_prime_balance(data["power"], cp=cp, algorithm='froncioni-skiba-clarke', w_prime=w_prime).to_list()
+# w_bal_waterworth = sweat.w_prime_balance(data["power"], cp=cp, algorithm='waterworth', w_prime=w_prime).to_list()
+# w_bal_fsc = sweat.w_prime_balance(data["power"], cp=cp, algorithm='froncioni-skiba-clarke', w_prime=w_prime).to_list()
 
-compare_power([w_bal_waterworth, w_bal_fsc], distance, legends=["waterworth", "froncioni-skiba-clarke"])
+# compare_power([w_bal_waterworth, w_bal_fsc], distance, legends=["waterworth", "froncioni-skiba-clarke"])
 
-compare_power([power, [cp]*len(power)], distance, legends=["power", "cp"])
+# compare_power([power, [cp]*len(power)], distance, legends=["power", "cp"])
 
 
+new_arr = remove_unactive_period(600, power, cadence, heart_rate, time)
+power, cadence, heart_rate, time = new_arr
+print(f"Average power is {np.average(power)}W")
+print(f"Average cadence is {np.average(cadence)}rpm")
+print(f"Max heartrate is {np.max(heart_rate)}bpm")
+print(f"Total time is {time[-1]}s")
 
 #plt.subplot(3,1,1)
 # plt.plot(distance, w_bal_waterworth)
---
+++ test_processing.py
@@ -0,0 +1,19 @@
+import numpy as np 
+
+
+def find_last_active_datapoint(power, approx_time):
+    for i in range(approx_time, len(power)):
+        if power[i] < 100:
+            return i
+
+def remove_unactive_period(approx_time, power, *args):
+    """
+    Remove unactive time in an activity. Unactive is defined as power below 100W.
+    
+    """
+    last_datapoint = find_last_active_datapoint(power, approx_time)
+    new_power = power[0:last_datapoint]
+    new_values = []
+    for elem in args:
+        new_values.append(elem[0:last_datapoint])
+    return (new_power,) + tuple(new_values)
