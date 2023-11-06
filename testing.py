28aee7d8ae776d7311a90cbe263c597715da4158
Henrik Brokke <brokkehenrik@gmail.com>
2023-11-06 16:57:32 +0100

    Implmented W-bal integral, doesnt work properly

--- testing.py
+++ testing.py
@@ -6,22 +6,69 @@ from plotting import *
 from activity_reader import ActivityReader
 from w_bal import *
 
-act = ActivityReader("350W_test.tcx")
-
+act = ActivityReader("Validation_test.tcx")
+act.remove_unactive_period(900)
 
 data = pd.DataFrame(dict(power=act.power), index=act.time)
 
 cp = 265
 w_prime = 26630
 
-# w_bal_waterworth = sweat.w_prime_balance(data["power"], cp=cp, algorithm='waterworth', w_prime=w_prime).to_list()
-# w_bal_fsc = sweat.w_prime_balance(data["power"], cp=cp, algorithm='froncioni-skiba-clarke', w_prime=w_prime).to_list()
+w_bal_waterworth = sweat.w_prime_balance(data["power"], cp=cp, algorithm='waterworth', w_prime=w_prime).to_list()
+w_bal_fsc = sweat.w_prime_balance(data["power"], cp=cp, algorithm='froncioni-skiba-clarke', w_prime=w_prime).to_list()
 
-# compare_power([w_bal_waterworth, w_bal_fsc], act.distance, legends=["waterworth", "froncioni-skiba-clarke"])
-# print(min(w_bal_fsc))
-# print(min(w_bal_waterworth))
+compare_power([w_bal_waterworth, w_bal_fsc], act.distance, legends=["waterworth", "froncioni-skiba-clarke"])
+print(min(w_bal_fsc))
+print(min(w_bal_waterworth))
 
-test = bi_exponential_w_bal(act.power, cp, w_prime)
-plt.plot(test)
-plt.show()
+# test = sweat.w_prime_balance(data["power"], cp=cp, algorithm='skiba', w_prime=w_prime).to_list()
+# test = bi_exponential_w_bal(act.power, cp, w_prime, 0.46)
+# plt.plot(test)
+# plt.show()
 
+# w_bal_skiba = sweat.w_prime_balance(data["power"], cp=cp, algorithm='waterworth', w_prime=w_prime).to_list()
+# plt.plot(w_bal_skiba)
+# plt.show()
+# work_bout_1 = act.power[3:248]
+# recovery_1 = act.power[248:248+240]
+# work_bout_2 = act.power[488:602]
+# recovery_2 = act.power[602:602+240]
+# work_bout_3 = act.power[842:len(act.power)]
+
+
+# for i, elem in enumerate([recovery_1, recovery_2]):
+#     print(f"Average power for recovery {i+1} was {np.average(elem)}\n")
+
+# for i, elem in enumerate([work_bout_1, work_bout_2, work_bout_3]):
+#     w_prime_exp = np.sum([power-cp for power in elem])
+#     print(f"Average power for work bout {i+1} is {np.average(elem)}W")
+#     print(f"W' expended for work bout {i+1} is {w_prime_exp/1000}kJ\n")
+
+
+
+
+# def count_above_or_below_threshold(arr, thres):
+#     diff = [power-thres for power in arr]
+#     print(diff)
+#     count = 1
+#     result = []
+#     for i in range(1, len(arr)):
+#         if diff[i] * diff[i-1] >0:
+#             count += 1
+#         else:
+#             for _ in range(count):
+#                 result.append(count)
+#             count = 1
+#     for _ in range(count):
+#         result.append(count)
+#     return result
+
+
+
+
+# arr = [300, 300, 200, 200, 200, 300, 299, 399, 300, 299, 399, 300, 299, 399]
+# new_arr = count_above_or_below_threshold(act.power, 250)
+# print(new_arr)
+
+# def count_streaks(arr, thres):
+#     diff = [power-thres for power in arr]
--- w_bal.py
+++ w_bal.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import math
 
 def differential_w_bal(power, cp, awc):
     w_bal = []
@@ -15,6 +15,30 @@ def differential_w_bal(power, cp, awc):
         w_bal_current = w_bal_next
 
     return w_bal       
+
+
+def integral_w_bal(power, cp, awc):
+    w_bal = []
+
+    for t in range(len(power)):
+        w_expended_total = 0
+        for u, p in enumerate(power[:t+1]):
+            w_expended = max(0, p-cp)
+            w_expended_total += w_expended *np.exp(-(t-u)/tau_w_prime_balance(power,cp))
+        w_bal.append(awc-w_expended_total)
+
+    return w_bal
+
+
+def tau_w_prime_balance(power, cp):
+    below_cp = [cp-p for p in power if p<cp]
+    avg_power_below_cp = np.average(below_cp)
+    if math.isnan(avg_power_below_cp):
+        avg_power_below_cp = 0
+    delta_cp = cp - avg_power_below_cp
+
+    return 546 * math.e ** (-0.01 * delta_cp) + 316
+
 
 def bi_conditional_w_bal(power, cp, awc):
     w_bal = []
@@ -40,42 +64,45 @@ def bi_conditional_w_bal(power, cp, awc):
         w_bal_current = w_bal_next
     return w_bal
 
-def bi_exponential_w_bal(power, cp, awc):
+def count_above_or_below_threshold(arr, thres):
+    diff = [power-thres for power in arr]
+    count = 1
+    result = []
+    for i in range(1, len(arr)):
+        if diff[i] * diff[i-1] >0:
+            count += 1
+        else:
+            for _ in range(count):
+                result.append(count)
+            count = 1
+    for _ in range(count):
+        result.append(count)
+    return result
+
+def bi_exponential_w_bal(power, cp, awc, recovery_param):
     w_bal = []
     w_bal_current = awc
     w_bal_next = 0
-    fc_amp = awc*(0.75*0.4+5.26)
+    fc_amp = awc*(0.75*recovery_param + 5.26)
     sc_amp = awc-fc_amp    
     # vet ikke hvordan disse skal defineres:
-    fc_bal = 0.5
-    sc_bal = 0.5
-    length_of_interval = np.zeros(len(power))
+    fc_bal = 0.000003
+    sc_bal = 0.00004
+    length_of_interval = count_above_or_below_threshold(power, cp)
 
     # todo: make an array that has the same length as the power output, and contains the length of the current exp/rec interval
-    for i in range(len(power)):
+    for t, p in enumerate(power):
 
-        if power[i] >= cp:
-            exp_interval = 0
-            for j in range(i,len(power)):
-                if power[j] < cp:
-                    exp_interval = j-i
-                    length_of_interval[i:exp_interval] = exp_interval
-                    break
-
-            w_bal_next = awc - ((max(0,power[i]-cp))*exp_interval*fc_bal)/w_bal_current - ((max(0,power[i]-cp))*exp_interval*sc_bal)/w_bal_current
+        if p >= cp:
+            w_bal_next = awc - ((max(0,p-cp))*t*fc_bal)/w_bal_current - ((max(0,p-cp))*t*sc_bal)/w_bal_current
         
         else:
-            for j in range(i,len(power)):
-                if power[j] > cp:
-                    rec_interval = j-i
- 
-                    length_of_interval[i:rec_interval] = rec_interval
-                    break
-            w_bal_next = (fc_amp-fc_bal)*(1-np.exp(-rec_interval/(45*np.exp(-0.014*(power[i]/cp))+9999*np.exp(-0.811*(power[i]/cp))))) + (sc_amp-sc_bal)*(1-np.exp(-rec_interval/(9999*np.exp(-0.098*(power[i]/cp))+429)))
+            w_bal_next = (fc_amp-fc_bal)*(1-np.exp(-t/(45*np.exp(-0.014*(p/cp))+9999*np.exp(-0.811*(p/cp))))) + (sc_amp-sc_bal)*(1-np.exp(-t/(9999*np.exp(-0.098*(p/cp))+429)))
         
         w_bal.append(w_bal_next)
         w_bal_current = w_bal_next
-        print(length_of_interval)
+
+    print(length_of_interval)
     return w_bal
 
 
