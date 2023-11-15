855b1ec0303a2a4817d7872a170cdb675834125a
Henrik Brokke <brokkehenrik@gmail.com>
2023-11-15 15:31:22 +0100

    Working on W'bal models

--- testing.py
+++ testing.py
@@ -6,27 +6,23 @@ from plotting import *
 from activity_reader import ActivityReader
 from w_bal import *
 
-act = ActivityReader("Validation_test_30s_rec.tcx")
-act.remove_unactive_period(400)
+# act = ActivityReader("Validation_test_30s_rec.tcx")
+# act.remove_unactive_period(400)
+power = [0] + 19*[300] + [20, 20]
+time = np.arange(0, 22, 1)
+cp = 245
+w_prime = 14000
+data = pd.DataFrame(dict(power=power), index=time)
 
-data = pd.DataFrame(dict(power=act.power), index=act.time)
+w_bal_ode = sweat.w_prime_balance(data["power"], cp=cp, algorithm='ode', w_prime=w_prime).to_list()
+w_bal_skiba = sweat.w_prime_balance(data["power"], cp=cp, algorithm='skiba', w_prime=w_prime, tau_dynamic=False).to_list()
 
-cp = 265
-w_prime = 26630
 
-w_bal_waterworth = sweat.w_prime_balance(data["power"], cp=cp, algorithm='waterworth', w_prime=w_prime).to_list()
-w_bal_fsc = sweat.w_prime_balance(data["power"], cp=cp, algorithm='froncioni-skiba-clarke', w_prime=w_prime).to_list()
+compare_power([w_bal_ode, w_bal_skiba], time, legends=["waterworth", "froncioni-skiba-clarke"])
 
-compare_power([w_bal_waterworth, w_bal_fsc], act.time, legends=["waterworth", "froncioni-skiba-clarke"])
-print(min(w_bal_fsc))
-print(min(w_bal_waterworth))
+for w_bal in w_bal_skiba:
+    print(w_bal)
 
-w_bal_bi_exp = bi_exponential_w_bal(act.power, cp, w_prime, 0.46)
-
-plt.plot(w_bal_bi_exp)
-
-plt.legend(['bi exp'])
-plt.show()
 
 # test = sweat.w_prime_balance(data["power"], cp=cp, algorithm='skiba', w_prime=w_prime).to_list()
 # test = integral_w_bal(act.power, cp, w_prime)
@@ -37,6 +33,17 @@ plt.show()
 # plt.plot(w_bal_skiba)
 # plt.show()
 
+cp = 265
+w_prime = 26630
 
+act = ActivityReader("Validation_test_240s_rec.tcx")
+act.remove_unactive_period(700)
+data = pd.DataFrame(dict(power=act.power), index=act.time)
 
+w_bal_bi_exp = sweat.w_prime_balance(data["power"], cp=cp, algorithm='dyamic_bi_exponential', w_prime=w_prime).to_list()
 
+plt.plot(act.time, w_bal_bi_exp)
+plt.show()
+
+plt.plot(act.time, act.power)
+plt.show()
--- w_bal.py
+++ w_bal.py
@@ -1,38 +1,14 @@
-import numpy as np
 import math
 
-def differential_w_bal(power, cp, awc):
-    w_bal = []
-    w_bal_current = awc
-    w_bal_next = 0
-
-    for p in power:
-        if p < cp:
-            w_bal_next = w_bal_current + (cp-p) * (awc-w_bal_current)/awc
-        else:
-            w_bal_next = w_bal_current + (cp-p)
-        w_bal.append(w_bal_next)
-        w_bal_current = w_bal_next
-
-    return w_bal       
+import numpy as np
+import pandas as pd
 
 
-def integral_w_bal(power, cp, awc):
-    w_bal = []
+def tau_w_prime_balance(power, cp, untill=None):
+    if untill is None:
+        untill = len(power)
 
-    for t in range(len(power)):
-        w_expended_total = 0
-        for u, p in enumerate(power[:t+1]):
-            w_expended = max(0, p-cp)
-            w_expended_total += w_expended *np.exp(-(t-u)/tau_w_prime_balance(power,cp))
-        w_bal.append(awc-w_expended_total)
-
-    return w_bal
-
-
-def tau_w_prime_balance(power, cp):
-    below_cp = [cp-p for p in power if p<cp]
-    avg_power_below_cp = np.average(below_cp)
+    avg_power_below_cp = power[:untill][power[:untill] < cp].mean()
     if math.isnan(avg_power_below_cp):
         avg_power_below_cp = 0
     delta_cp = cp - avg_power_below_cp
@@ -40,69 +16,220 @@ def tau_w_prime_balance(power, cp):
     return 546 * math.e ** (-0.01 * delta_cp) + 316
 
 
-def bi_conditional_w_bal(power, cp, awc):
-    w_bal = []
-    w_bal_current = awc
-    w_bal_next = 0
-    w_exp = 0
-    diff_above = np.zeros(len(power))
-    diff_below = np.zeros(len(power))
+def get_tau_method(power, cp, tau_dynamic, tau_value):
+    if tau_dynamic:
+        tau_dynamic = [tau_w_prime_balance(power, cp, i) for i in range(len(power))]
+        tau = lambda t: tau_dynamic[t]
 
-    for i in range(len(power)):
-        if power[i] >= cp:
-            diff_above[i] = power[i] - cp
+    elif tau_value is None:
+        static_tau = tau_w_prime_balance(power, cp)
+        tau = lambda t: static_tau
+
+    else:
+        tau = lambda t: tau_value
+
+    return tau
+
+
+def get_bi_exp_tau_method(power, cp, tau_dynamic, tau_value, fast_component):
+    if tau_dynamic:
+        if fast_component:
+            tau_dynamic = [tau_fc(power, cp, i) for i in range(len(power))]
+            tau = lambda t: tau_dynamic[t]
         else:
-            diff_below[i] = cp - power[i]
+            tau_dynamic = [tau_sc(power, cp, i) for i in range(len(power))]
+            tau = lambda t: tau_dynamic[t]
 
-    for i in range(len(power)):
-        if power[i] >= cp:
-            w_bal_next = w_bal_current - (power[i]-cp)*diff_above[i]
-            w_exp = (power[i]-cp)*diff_above[i]
-        else:  
-            w_bal_next = w_bal_current - w_exp*np.exp(-diff_below[i]/(546*np.exp(-0.01*(cp-np.average(diff_below)))+316))
-        w_bal.append(w_bal_next)
-        w_bal_current = w_bal_next
-    return w_bal
-
-def count_above_or_below_threshold(arr, thres):
-    diff = [power-thres for power in arr]
-    count = 1
-    result = []
-    for i in range(1, len(arr)):
-        if diff[i] * diff[i-1] >0:
-            count += 1
+    elif tau_value is None:
+        if fast_component:
+            static_tau = tau_fc(power, cp)
+            tau = lambda t: static_tau
         else:
-            for _ in range(count):
-                result.append(count)
-            count = 1
-    for _ in range(count):
-        result.append(count)
-    return result
+            static_tau = tau_sc(power, cp)
+            tau = lambda t: static_tau
 
-def bi_exponential_w_bal(power, cp, awc, recovery_param):
-    w_bal = []
-    w_bal_current = awc
-    w_bal_next = 0
-    fc_amp = awc*(0.75*recovery_param + 5.26)
-    sc_amp = awc-fc_amp    
-    # vet ikke hvordan disse skal defineres:
-    fc_bal = 0.5
-    sc_bal = 0.5
-    length_of_interval = count_above_or_below_threshold(power, cp)
+    else:
+        tau = lambda t: tau_value
 
-    # todo: make an array that has the same length as the power output, and contains the length of the current exp/rec interval
+    return tau
+
+def tau_fc(power, cp, untill = None):
+    if untill is None:
+        untill = len(power)
+
+    avg_power_below_cp = power[:untill][power[:untill] < cp].mean()
+    if math.isnan(avg_power_below_cp):
+        avg_power_below_cp = 0
+    d_cp = avg_power_below_cp/cp
+
+    return 45 * math.e ** (-0.014 * d_cp) + 9999 * math.e ** (-0.811 * d_cp)
+
+
+def tau_sc(power, cp, untill = None):
+    if untill is None:
+        untill = len(power)
+
+    avg_power_below_cp = power[:untill][power[:untill] < cp].mean()
+    if math.isnan(avg_power_below_cp):
+        avg_power_below_cp = 0
+    d_cp = avg_power_below_cp/cp
+
+    return 9999* math.e ** (-0.098 * d_cp) + 429
+
+
+def w_prime_bal_dynamic_bi_exp(power, cp, w_prime, rec_parameter=0.46, tau_dynamic=False, tau_value=None, *args, **kwargs):
+    last = w_prime
+    w_prime_balance = []
+    FC_amp = w_prime * (0.75 * rec_parameter + 5.26)
+    SC_amp = w_prime - FC_amp
+    tau_fc = get_bi_exp_tau_method(power, cp, tau_dynamic, tau_value, fast_component=True)
+    tau_sc = get_bi_exp_tau_method(power, cp, tau_dynamic, tau_value, fast_component=False)
+
+    for t in range(len(power)):
+
+        for u, p in enumerate(power[: t + 1]):
+            if p >= cp: 
+                new = w_prime - ((p-cp) * t * w_prime * FC_amp / last) - ((p-cp) * t * w_prime*SC_amp / last) 
+            else: 
+                new = (FC_amp - w_prime*FC_amp) * (1 - math.e ** (-t/tau_fc(t))) + (SC_amp - w_prime*SC_amp) * (1 - math.e ** (-t/tau_sc(t)))
+                
+
+        w_prime_balance.append(new)
+        last = new
+
+    return pd.Series(w_prime_balance)
+
+
+def w_prime_balance_waterworth(
+    power, cp, w_prime, tau_dynamic=False, tau_value=None, *args, **kwargs
+):
+    """
+    Optimisation of Skiba's algorithm by Dave Waterworth.
+    Source:
+    http://markliversedge.blogspot.nl/2014/10/wbal-optimisation-by-mathematician.html
+    Source:
+    Skiba, Philip Friere, et al. "Modeling the expenditure and reconstitution of work capacity above critical power." Medicine and science in sports and exercise 44.8 (2012): 1526-1532.
+    """
+    sampling_rate = 1
+    running_sum = 0
+    w_prime_balance = []
+    tau = get_tau_method(power, cp, tau_dynamic, tau_value)
+
     for t, p in enumerate(power):
+        power_above_cp = p - cp
+        w_prime_expended = max(0, power_above_cp) * sampling_rate
+        running_sum = running_sum + w_prime_expended * (
+            math.e ** (t * sampling_rate / tau(t))
+        )
 
-        if p >= cp:
-            w_bal_next = awc - ((max(0,p-cp))*t*fc_bal)/w_bal_current - ((max(0,p-cp))*t*sc_bal)/w_bal_current
-        
+        w_prime_balance.append(
+            w_prime - running_sum * math.e ** (-t * sampling_rate / tau(t))
+        )
+
+    return pd.Series(w_prime_balance)
+
+
+def w_prime_balance_skiba(
+    power, cp, w_prime, tau_dynamic=False, tau_value=None, *args, **kwargs
+):
+    """
+    Source:
+    Skiba, Philip Friere, et al. "Modeling the expenditure and reconstitution of work capacity above critical power." Medicine and science in sports and exercise 44.8 (2012): 1526-1532.
+    """
+    w_prime_balance = []
+    tau = get_tau_method(power, cp, tau_dynamic, tau_value)
+
+    for t in range(len(power)):
+        w_prime_exp_sum = 0
+
+        for u, p in enumerate(power[: t + 1]):
+            w_prime_exp = max(0, p - cp)
+            w_prime_exp_sum += w_prime_exp * np.power(np.e, (u - t) / tau(t))
+
+        w_prime_balance.append(w_prime - w_prime_exp_sum)
+
+    return pd.Series(w_prime_balance)
+
+
+def w_prime_balance_bi_exp(
+    power, cp, w_prime, tau_dynamic=False, tau_value=None, *args, **kwargs
+):
+
+    w_prime_balance = []
+    tau_1 = get_tau_method(power, cp, tau_dynamic, 22)
+    tau_2 = get_tau_method(power, cp, tau_dynamic, 377)
+    A = 1
+    alpha_1 = 0.477
+    alpha_2 = 1-alpha_1
+
+    for t in range(len(power)):
+        w_prime_exp_sum = 0
+
+        for u, p in enumerate(power[: t + 1]):
+            w_prime_exp = max(0, p - cp)
+            w_prime_exp_sum += w_prime_exp * (A*alpha_1 * np.power(np.e, (u - t) / tau_1(t)) +  A*alpha_2 * np.power(np.e, (u - t) / tau_2(t)))
+
+        w_prime_balance.append(w_prime - w_prime_exp_sum)
+
+    return pd.Series(w_prime_balance)
+
+
+def w_prime_balance_froncioni_skiba_clarke(power, cp, w_prime):
+    """
+    Source:
+    Skiba, P. F., Fulford, J., Clarke, D. C., Vanhatalo, A., & Jones, A. M. (2015). Intramuscular determinants of the ability to recover work capacity above critical power. European journal of applied physiology, 115(4), 703-713.
+    """
+    last = w_prime
+    w_prime_balance = []
+
+    for p in power:
+        if p < cp:
+            new = last + (cp - p) * (w_prime - last) / w_prime
         else:
-            w_bal_next = (fc_amp-fc_bal)*(1-np.exp(-t/(45*np.exp(-0.014*(p/cp))+9999*np.exp(-0.811*(p/cp))))) + (sc_amp-sc_bal)*(1-np.exp(-t/(9999*np.exp(-0.098*(p/cp))+429)))
-        
-        w_bal.append(w_bal_next)
-        w_bal_current = w_bal_next
+            new = last + (cp - p)
 
-    return w_bal
+        w_prime_balance.append(new)
+        last = new
+
+    return pd.Series(w_prime_balance)
+
+
+def w_prime_balance_ode(power, cp, w_prime):
+
+    last = w_prime
+    w_prime_balance = []
+
+    for p in power:
+        if p < cp:
+            new = w_prime - (w_prime - last) * np.power(np.e, -(cp - p)/w_prime)
+
+        elif p == cp:
+            new = last
+        else:
+            new = last - (p - cp)
+
+        w_prime_balance.append(new)
+        last = new
+
+    return pd.Series(w_prime_balance)
+
+
+def w_prime_balance(power, cp, w_prime, algorithm="waterworth", *args, **kwargs):
+    if algorithm == "waterworth":
+        method = w_prime_balance_waterworth
+    elif algorithm == "skiba":
+        method = w_prime_balance_skiba
+    elif algorithm == "froncioni-skiba-clarke":
+        method = w_prime_balance_froncioni_skiba_clarke
+    elif algorithm == "ode":
+        method = w_prime_balance_ode
+    elif algorithm == "bi_exponential":
+        method = w_prime_balance_bi_exp
+    elif algorithm == "dyamic_bi_exponential":
+        method = w_prime_bal_dynamic_bi_exp
+
+    return method(power, cp, w_prime, *args, **kwargs)
+
 
 
         
